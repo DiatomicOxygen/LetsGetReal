cf19b5a2b05d849fa19bd15791d7ef15057782a2
DiatomicOxygen <eoo20@stuy.edu>
2020-11-20 18:29:22 -0500

    RationalNumber constructor works

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,17 @@
-public class RationalNumber {
-  
+public class RationalNumber extends RealNumber {
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno){
+    super(0.0);
+    numerator = nume;
+    denominator = deno;
+    if (nume < 0 || deno < 0) {
+      numerator = Math.abs(nume) * -1;
+      denominator = Math.abs(deno) * -1;
+    }
+    if (deno == 0) {
+      denominator = 1;
+      numerator = 0;
+    }
+  }
 }
