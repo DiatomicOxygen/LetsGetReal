66dfdba57416487e747d97e3bfdde3b12980d91f
DiatomicOxygen <eoo20@stuy.edu>
2020-11-26 12:54:48 -0500

    added compareTo

--- Number.java
+++ Number.java
@@ -1,5 +1,15 @@
 public abstract class Number {
   public abstract double getValue();
 
+  public int compareTo(Number other){
+    if (this.getValue() == other.getValue()) {
+      return 0;
+    }
+    if (this.getValue() >= other.getValue()) {
+      return 1;
+    }
+    return -1;
+  }
+
   
 }
